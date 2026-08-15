c2582031ac56087d1137c8dffbf2fdfe1c4988f9
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2026-08-16 00:51:34 +0700

    chore(bootstrap): publishAddress as a const

--- index.mjs
+++ index.mjs
@@ -4,10 +4,19 @@ import { pool } from './lib/db.mjs';
 import initClusterSchema from './lib/clusters/db-init.mjs';
 import { electionLoop } from './lib/clusters/elections.mjs';
 
-logger.info('Starting...');
+const module = 'MAIN';
+
+const publishAddress = process.env.SILOKA_PUBLISH_ADDRESS;
+
+logger.info({
+  module,
+  eventId: '21DD5B7C',
+  publishAddress
+
+}, 'Starting...');
 await initClusterSchema(pool);
 electionLoop(
-  process.env.SILOKA_PUBLISH_ADDRESS,
+  publishAddress,
   Number(process.env.SILOKA_MASTER_LEADER_TTL_SECS) || undefined,
   Number(process.env.SILOKA_MASTER_ELECTION_INTERVAL_MS) || undefined
 );
