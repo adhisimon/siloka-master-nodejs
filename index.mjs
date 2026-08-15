6a294cc13166e4121fa5b8eafc73b2ec0c680b87
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2026-08-15 22:45:02 +0700

    chore: basic bootstraping

--- index.mjs
+++ index.mjs
@@ -1,3 +1,6 @@
 import logger from './lib/logger.mjs';
+import { pool } from './lib/db.mjs';
+import initClusterSchema from './lib/clusters/db-init.mjs';
 
 logger.info('Starting...');
+await initClusterSchema(pool);
