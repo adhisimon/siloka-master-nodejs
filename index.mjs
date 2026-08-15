2339de5af8fcdcc4264092b9c1158306ea8e2e13
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2026-08-16 00:34:20 +0700

    feat(cluster): implement leader election loop

--- index.mjs
+++ index.mjs
@@ -1,6 +1,13 @@
+import 'dotenv/config';
 import logger from './lib/logger.mjs';
 import { pool } from './lib/db.mjs';
 import initClusterSchema from './lib/clusters/db-init.mjs';
+import { electionLoop } from './lib/clusters/elections.mjs';
 
 logger.info('Starting...');
 await initClusterSchema(pool);
+electionLoop(
+  process.env.SILOKA_PUBLISH_ADDRESS,
+  Number(process.env.SILOKA_MASTER_LEADER_TTL_SECS) || undefined,
+  Number(process.env.SILOKA_MASTER_ELECTION_INTERVAL_MS) || undefined
+);
